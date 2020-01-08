e62dd5941925abdfda451634b5d0b27b7282cea4
lazinferisl80 <isl.ivan.ivan@gmail.com>
2020-01-08 19:46:22 +0100

    ok

---
+++ app.js
@@ -0,0 +1,13 @@
+var matriz = ["rojo", "azul", "amarillo", "verde", "blanco", "negro",
+
+[1, 2, 3, 4, 5, 6, 
+    ["rojo1", "azul2", "amarillo3", "verde4", "blanco5"," negro6"]
+]
+];
+
+alert(matriz.length);
+
+alert(matriz[6][6][3]);
+
+alert(matriz.length)
+console.log(matriz)
